d8e700ff73f9dc344a6366ac92fcd223f2a3e9ef
xiaowei3 <793089768@qq.com>
2017-06-17 23:00:53 +0800

    “finish skip_list and test innerClass properties”

--- LetCode/src/com/xiaowei/com/xiaowei/test/Singleton.java
+++ LetCode/src/com/xiaowei/com/xiaowei/test/Singleton.java
@@ -7,6 +7,17 @@ package com.xiaowei.com.xiaowei.test;
  * 1.类中的静态代码块和静态变量按顺序加载
  * 2.静态内部类编译之后也是一个class文件，所以只有需要使用该类时，才会被加载，加载的顺序和普通类一致
  *
+ *  测试内部类对象的创建方式及外部类访问内部类私有成员的权限
+ *
+ *  结论：
+ *  1. 静态内部类创建对象方式-- new outClass.innerClass();
+ *  2.非静态内部类创建对象方式-- outClass out = new outClass();  outClass.innerClass inner = out.new innerClass();
+ *  3.内部类可以理解为外部类的成员，所以外部类和内部类可以互相访问所有成员（包含私有成员），遵循成员互访原则
+ *
+ *  4.静态内部类可以含有静态方法和静态变量，静态变量和静态方法可以直接访问,如：outClass.innerClass.static_function();
+ *  5.非静态内部类不可以含有静态方法和静态变量（可以从类加载顺序来理解为什么)
+ *
+ *
  * Created by zhangwei on 2017/6/17.
  */
 public class Singleton {
@@ -34,9 +45,48 @@ public class Singleton {
         System.out.println("Test Static");
     }
 
+    //静态内部类
+    static class static_inner_class{
+        private int num;
+        public static_inner_class(){
+            num= 10;
+        }
+
+        public static void print_static() {
+            System.out.println("this is inner class static function");
+        }
+
+        public void nonStatic_print() {
+            System.out.println("this is inner class nonStatic function");
+        }
+    }
+    //非静态内部类
+    class nonStatic_inner_class{
+        private int num;
+        public nonStatic_inner_class(){
+            num= 10;
+        }
+        public void print() {
+            System.out.println("this is inner class nonStatic function");
+        }
+    }
+
     public static void main(String args[]){
         Singleton t = new Singleton(2);
 //        Singleton.getInstance();
+
+
+//        //测试创建非静态内部类对象的方式
+        Singleton.nonStatic_inner_class nsic =  t.new nonStatic_inner_class();
+        System.out.println(nsic.num);
+
+
+//        //测试静态内部类new 对象的方式
+//        Singleton.static_inner_class sic = new Singleton.static_inner_class();
+//        System.out.println(sic.num);
+//        //静态内部类的静态方法可以直接访问
+//        Singleton.static_inner_class.print_static();
+
     }
 
 }
--- LetCode/src/com/xiaowei/design/skip_list.java
+++ LetCode/src/com/xiaowei/design/skip_list.java
@@ -1,32 +1,173 @@
 package com.xiaowei.design;
 
+import java.util.Random;
+
 /**
- * Created by leetcode on 2017/6/12.
+ * the skip_list max level can only be MAXLEVEL,
+ * when it set over the value of MAXLEVEL,it will be setted default vaule with MAXLEVEL
+ * Created by zhangwei on 2017/6/12.
  */
 
-public class skip_list<valueType> {
+public class skip_list<E extends Comparable<? super E>> {
+    private Node<E> head;// 指向头节点
+    private int level; //设置该链表的最大层数
+    private int len; //链表的长度
+    private Random rand = new Random();
+    //链表可设置的最大层数阈值
+    private final static int MAXLEVEL = 63;
 
-    private int level; //层次
-    private Node<valueType> head;// 指向头节点
-
-    private final static int DEFAULT_LEVEL =  3;
-    private Node<valueType> nil;  //结束标志
-    private Node<valueType> current; // 当前位置
-
-
-    /**
-     * 跳表节点定义
-     */
-    class Node<valueType>{
-        private valueType value; // 节点的值
-        private Node[] forward ; //指向下一个节点的数组
-
-        public Node( valueType value,int level_num){
-            this.value = value;
-            this.forward = new Node[level_num];
-        }
+    public skip_list(int level){
+        if(level < 2) level = 2;
+        else if(level>MAXLEVEL) level = MAXLEVEL;
+        this.level = level;
+        head = new Node<>(null,level);
     }
 
+    /**
+     * 添加一个新节点
+     * @param val
+     */
+    public void add(E val) {
+        //确定新节点的level
+        int nodeLevel = chooseLevel(this.level);
+        //创建新节点
+        Node<E> node = new Node<E>(val, level);
+        //添加到skip_list中
+        Node<E> curNode = head;
+        int curLevel = nodeLevel -1;
+        while (curLevel >= 0) {
+            while (curNode.getNext(curLevel) != null && curNode.getNext(curLevel).getValue().compareTo(val) < 0) {
+                curNode = curNode.getNext(curLevel);
+            }
+            node.setNext(curLevel,curNode.getNext(curLevel));
+            curNode.setNext(curLevel,node);
+            curLevel--;
+        }
+        this.len++;
+    }
+
+    /**
+     * 从链表中删除一个元素
+     * @param val
+     */
+    public void remove(E val) {
+        Node<E> curNode = this.head;
+        int curLevel = this.level-1;
+        boolean isExist = false;
+        while (curLevel >= 0) {
+            if (curNode.getNext(curLevel) != null && curNode.getNext(curLevel).getValue().compareTo(val) > 0) {
+                curLevel--;
+            } else if (curNode.getNext(curLevel).getValue().compareTo(val) < 0) {
+                curNode = curNode.getNext(curLevel);
+            }else{
+                curNode.setNext(curLevel,curNode.getNext(curLevel).getNext(curLevel));
+                curLevel--;
+                isExist = true;
+            }
+        }
+        if(isExist) this.len--;
+    }
+
+    /**
+     * 判断链表中是否存在某个元素
+     * @param val
+     * @return
+     */
+    public boolean contains(E val) {
+        Node<E> curNode = this.head;
+        int curLevel = this.level-1;
+        while (curLevel >= 0) {
+            if (curNode.getNext(curLevel) == null || curNode.getNext(curLevel).getValue().compareTo(val) > 0) {
+                curLevel--;
+            } else if (curNode.getNext(curLevel).getValue().compareTo(val) < 0) {
+                curNode = curNode.getNext(curLevel);
+            }else{//found
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 获取指定下标元素的值
+     * @param index
+     * @return
+     */
+    public E get(int index) {
+        Node<E> curNode = this.head;
+        while (index >= 0) {
+            curNode = curNode.getNext(0);
+            index--;
+        }
+        return curNode.getValue();
+    }
+
+    /**
+     * 获取链表的长度
+     * @return
+     */
+    public int size(){
+        return this.len;
+    }
+    //确定新节点的层次
+    private int chooseLevel(int level) {
+        long n = (long) 1 << (level - 1);
+        long ranNum;
+        if (level < 32) {
+            //ranNum范围[1,n]
+            ranNum = rand.nextInt((int) n) + 1;
+        }else{
+            ranNum = rand.nextLong();
+            if (ranNum < 0) {
+                //ranNum范围[1,n]
+                ranNum = (ranNum + Long.MAX_VALUE)%n + 1;
+            } else if (ranNum > n) {
+                //ranNum范围[1,n]
+                ranNum = ranNum % n + 1;
+            }
+        }
+        int ranLevel = 1;
+        while (true) {
+            n >>= 1;
+            if (ranNum > n) return ranLevel;
+            ranLevel++;
+        }
+    }
+}
+
+/**
+ * 跳表节点定义
+ */
+class Node<E extends Comparable<? super E>>{
+    private E value; // 节点的值
+    private Node[] forward; //指向下一个节点的数组
+    //构造函数
+    public Node( E value,int level_num){
+        this.value = value;
+        this.forward = new Node[level_num];
+    }
+
+    //获取节点的值
+    public E getValue(){
+        return value;
+    }
+
+    public Node getNext(int curLevel) {
+        return forward[curLevel];
+    }
+
+    public void setNext(int curLevel,Node node) {
+        this.forward[curLevel] = node;
+    }
+
+    //获取层数
+    public int levelNum() {
+        return forward.length-1;
+    }
+
+    public String toString() {
+        return "SLV："+value;
+    }
 }
 
 
